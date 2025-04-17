d56c37d1bda3ad6265a3169fd39651dcd05efebe
TANG <m136070018@student.nsysu.edu.tw>
2025-04-17 13:25:40 +0800

    Apple美國售價中位數與標準差

--- 期中考.R
+++ 期中考.R
@@ -4,3 +4,7 @@ library(ggplot2)
 
 setwd("C:/Users/dcukf/Desktop/R語言/")
 M<- read.csv("Mobiles.csv", header=TRUE)
+
+appleA <- subset(M, Company=="Apple")
+mean(appleA$PriceUSA)   #Apple美國售價平均數1028.485
+sd(appleA$PriceUSA)   #Apple美國售價標準差247.8969
