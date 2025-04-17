e615ab58bd2d6e666f049c7c0b2211f467a4fb1c
TANG <m136070018@student.nsysu.edu.tw>
2025-04-17 14:15:11 +0800

    再更

--- 期中考.R
+++ 期中考.R
@@ -19,3 +19,27 @@ boxplot(Year~PriceChina, data=M,
         xlab="年份",ylab="價格",main="年份-中國售價的盒鬚圖")  #年份-中國售價的盒鬚圖
 
 hist(M$PriceChina,xlab="人數",ylab="售價",main="中國售價長條圖")  #中國售價長條圖
+
+#第三題
+ggplot(M, mapping= aes(x=Weight, y=Battery))+
+  geom_point(color="skyblue",size=5,pch=8)+
+  ggtitle("手機重量-電池容量" , subtitle="點陣圖")+
+  xlab("weight")+ylab("battery")  #ggplot2 套件畫出手機重量-電池容量的點陣圖
+
+#第四題
+library(car)
+leveneTest(PriceUSA~Company, data=M, center = mean)
+
+#第五題
+run1<-aov(Weight~Company, data=M)
+summary(run1)  
+#廠牌對手機重量為非常顯著，呈高度正相關
+
+#第六題
+run2<-aov(Weight~Company+Battery, data=M)
+summary(run2)
+#廠牌和電池容量對手機重量都為非常顯著，呈高度正相關
+
+#第七題
+nrow(subset(YA,Year==2024 & Company=="Apple"))/nrow(YA)  
+#2024年發行手機中Apple比例為0.12
